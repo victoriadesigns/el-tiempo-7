128f2e511f6b4e92094838c4fcde627727e9daf4
victoriadesigns <156847173+victoriadesigns@users.noreply.github.com>
2024-01-17 20:26:39 -0800

    Updated the search function to include current temperature as well, prepped HTML for changing wind speed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function refreshWeather(response){
+    let displayedTemp = document.querySelector("#current-temp");
+    temperature = response.data.temperature.current;
+
+    let cityElement = document.querySelector("#displayedcity");
+    cityElement.innerHTML = response.data.city;
+
+    
+
+    console.log(response)
+
+    displayedTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city){
+    let apiKey = "4454f13co397b7eaaf38b607eb9367t6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#typed-city");
-    let cityElement = document.querySelector("#displayedcity");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
   
   let searchForm = document.querySelector("#city-form");
