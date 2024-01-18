1cbff6329d90e4b4febd8d35c3247662aecc2213
victoriadesigns <156847173+victoriadesigns@users.noreply.github.com>
2024-01-18 11:24:22 -0800

    Got the icon element hooked up to API

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ function refreshWeather(response){
     let cityElement = document.querySelector("#displayedcity");
     cityElement.innerHTML = response.data.city;
 
-    let icon = `<img src="${response.data.condition.icon_url}" />`;
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
     let descriptionElement = document.querySelector("#description");
     descriptionElement.innerHTML = response.data.condition.description;
@@ -21,28 +22,28 @@ function refreshWeather(response){
 }
 
 function formatDate(date){
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
-    let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-      }
-      
-      if (hours < 10) {
-        hours = `0${hours}`;
-      }
-    
-    return `${day}, ${hours}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  
+  if (hours < 10) {
+      hours = `0${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city){
