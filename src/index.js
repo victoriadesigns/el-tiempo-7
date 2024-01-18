6b75d328bf7ed9e78f8f187993adef65777eb918
victoriadesigns <156847173+victoriadesigns@users.noreply.github.com>
2024-01-17 21:26:52 -0800

    Trying to get an icon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ function refreshWeather(response){
     let cityElement = document.querySelector("#displayedcity");
     cityElement.innerHTML = response.data.city;
 
+    let icon = 
+    <img src="${response.condition.icon_url}" />;
+
     let descriptionElement = document.querySelector("#description");
     descriptionElement.innerHTML = response.data.condition.description;
 
@@ -15,13 +18,11 @@ function refreshWeather(response){
     let date = new Date(response.data.time * 1000);
     timeElement.innerHTML = formatDate(date);
 
-    console.log(response.data);
-
     displayedTemp.innerHTML = Math.round(temperature);
 }
 
 function formatDate(date){
-    let minutes = date.getMin();
+    let minutes = date.getMinutes();
     let hours = date.getHours();
     let days = [
         "Sunday",
@@ -33,6 +34,14 @@ function formatDate(date){
         "Saturday",
     ];
     let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+      
+      if (hours < 10) {
+        hours = `0${hours}`;
+      }
     
     return `${day}, ${hours}:${minutes}`;
 }
