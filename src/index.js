8c50bbd75000fcada569797d683dc6f751277ff6
victoriadesigns <156847173+victoriadesigns@users.noreply.github.com>
2024-01-19 12:59:50 -0800

    Changed icon CSS & integrated API into the JS

--- src/index.js
+++ src/index.js
@@ -60,6 +60,13 @@ function handleSearch(event) {
     searchCity(searchInput.value);
   }
 
+function formatDay(timestamp){
+  let date = new Date (timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city){
   let apiKey = "4454f13co397b7eaaf38b607eb9367t6";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -70,32 +77,35 @@ function displayForecast(response) {
 
     console.log(response.data);
 
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
   
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5){
       forecastHtml =
         forecastHtml +
         `
         <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <div>
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+          </div>
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15º</strong>
+              <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-            <div class="weather-forecast-temperature">9º</div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
           </div>
         </div>
       `;
+      }
     });
   
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
-  } 
+} 
   
-  let searchForm = document.querySelector("#city-form");
-  searchForm.addEventListener("submit", handleSearch);
+let searchForm = document.querySelector("#city-form");
+searchForm.addEventListener("submit", handleSearch);
 
 searchCity("Sunnyvale")
   
